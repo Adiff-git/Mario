331d4a5e519f3bee0f7028be0807d72b67fe072d
ThuanVien <24125051@student.hcmus.edu.vn>
2025-07-03 19:07:30 +0700

    test

--- build/raylib_build/raylib/CTestTestfile.cmake
+++ build/raylib_build/raylib/CTestTestfile.cmake
@@ -1,6 +1,6 @@
 # CMake generated Testfile for 
-# Source directory: C:/raylib/raylib/src
-# Build directory: D:/Downloads/Ref/Gia/Mario/build/raylib_build/raylib
+# Source directory: C:/raylib/src
+# Build directory: D:/prj3/Mario/build/raylib_build/raylib
 # 
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
