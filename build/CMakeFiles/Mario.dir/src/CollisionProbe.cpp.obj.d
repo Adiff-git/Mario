4f2c514df2efa0d17fa005f3f3169ee90c625054
Yeennhy <yennhii06072006@gmail.com>
2025-07-16 12:19:59 +0700

    commit

--- build/CMakeFiles/Mario.dir/src/CollisionProbe.cpp.obj.d
+++ build/CMakeFiles/Mario.dir/src/CollisionProbe.cpp.obj.d
@@ -1,12 +1,12 @@
 CMakeFiles/Mario.dir/src/CollisionProbe.cpp.obj: \
- D:\Downloads\Ref\Test\Mario\src\CollisionProbe.cpp \
- D:/Downloads/Ref/Test/Mario/inc/CollisionProbe.h \
+ D:\Mario_Gr02\src\CollisionProbe.cpp D:/Mario_Gr02/inc/CollisionProbe.h \
  C:/raylib/raylib/src/raylib.h \
- C:/msys64/ucrt64/lib/gcc/x86_64-w64-mingw32/14.2.0/include/stdarg.h \
- C:/msys64/ucrt64/include/stdarg.h \
- C:/msys64/ucrt64/include/_mingw_stdarg.h \
- C:/msys64/ucrt64/include/vadefs.h C:/msys64/ucrt64/include/_mingw.h \
- C:/msys64/ucrt64/include/_mingw_mac.h \
- C:/msys64/ucrt64/include/_mingw_secapi.h \
- C:/msys64/ucrt64/include/sdks/_mingw_ddk.h \
- D:/Downloads/Ref/Test/Mario/inc/Drawable.h
+ C:/mingw64/lib/gcc/x86_64-w64-mingw32/14.2.0/include/stdarg.h \
+ C:/mingw64/x86_64-w64-mingw32/include/stdarg.h \
+ C:/mingw64/x86_64-w64-mingw32/include/_mingw_stdarg.h \
+ C:/mingw64/x86_64-w64-mingw32/include/vadefs.h \
+ C:/mingw64/x86_64-w64-mingw32/include/_mingw.h \
+ C:/mingw64/x86_64-w64-mingw32/include/_mingw_mac.h \
+ C:/mingw64/x86_64-w64-mingw32/include/_mingw_secapi.h \
+ C:/mingw64/x86_64-w64-mingw32/include/sdks/_mingw_ddk.h \
+ D:/Mario_Gr02/inc/Drawable.h
--- build/raylib_build/CTestTestfile.cmake
+++ build/raylib_build/CTestTestfile.cmake
@@ -1,6 +1,6 @@
 # CMake generated Testfile for 
 # Source directory: C:/raylib/raylib
-# Build directory: D:/Downloads/Ref/Test/Mario/build/raylib_build
+# Build directory: D:/Mario_Gr02/build/raylib_build
 # 
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
