36ca8bb45601a8eca3b8f73075c2b593d7f48935
Nguyen Hoang Gia <hoanggia8765@gmail.com>
2025-08-06 15:17:34 +0700

    Add nv Peach Toad

--- inc/Character/Peach.h
+++ inc/Character/Peach.h
@@ -1,1 +1,22 @@
 #pragma once
+#include "Character.h"
+#include "../inc/Command/InputHandler.h"
+
+
+class Peach : public Character {
+private:
+   
+
+public:
+    Peach(Vector2 pos, int lives, ObjectState form, ControlType controlType)
+        : Character(pos, lives, form,  controlType) {
+        
+    }
+
+    
+
+    ~Peach() override = default;
+
+    void Update() override;
+
+};
---
+++ src/Character/Peach.cpp
@@ -0,0 +1,183 @@
+#include "../inc/Character/Peach.h"
+#include "ResrcManager.h"
+#include "../inc/World/GameClock.h"
+
+void Peach::Update() {
+    const float deltaTime = GetFrameTime();
+    if (state == OBJECT_STATE_TO_BE_REMOVED) {
+        sprite = nullptr;
+        return;
+    }
+    if (state == OBJECT_STATE_DYING) {
+        sprite = &ResrcManager::GetInstance().getTexture("PEACH_DIE");
+        return;
+    }
+
+    switch(CharState) { // Corrected from PEACHState to PEACHState
+        case SMALL:
+        {
+            if(state == OBJECT_STATE_ON_GROUND) {
+                if (vel.x != 0 && !isDucking) {
+                    if ( direction == DIRECTION_RIGHT) {
+                        if(currentFrame == 0) {
+                            sprite = &ResrcManager::GetInstance().getTexture("SMALLPEACH_0_RIGHT");
+                        } else if(currentFrame == 1) {
+                            sprite = &ResrcManager::GetInstance().getTexture("SMALLPEACH_1_RIGHT");
+                        }
+                    }
+                    else  {
+                        if(currentFrame == 0) {
+                            sprite = &ResrcManager::GetInstance().getTexture("SMALLPEACH_0_LEFT");
+                        } else if(currentFrame == 1) {
+                            sprite = &ResrcManager::GetInstance().getTexture("SMALLPEACH_1_LEFT");
+                        }
+                    }
+                }
+                if(vel.x == 0 && !isDucking) {
+                    if ( direction == DIRECTION_RIGHT) {
+                        sprite = &ResrcManager::GetInstance().getTexture("SMALLPEACH_0_RIGHT");
+                    } else if ( direction == DIRECTION_LEFT) {
+                        sprite = &ResrcManager::GetInstance().getTexture("SMALLPEACH_0_LEFT");
+                    }
+                }
+            }
+
+            if (state == OBJECT_STATE_JUMPING) {
+                if (direction == DIRECTION_RIGHT) {
+                    sprite = &ResrcManager::GetInstance().getTexture("SMALLPEACH_JUMPING_RIGHT");
+                } else if (direction == DIRECTION_LEFT) {
+                    sprite = &ResrcManager::GetInstance().getTexture("SMALLPEACH_JUMPING_LEFT");
+                }
+            }
+
+            if (state == OBJECT_STATE_FALLING) {
+                if (direction == DIRECTION_RIGHT) {
+                    sprite = &ResrcManager::GetInstance().getTexture("SMALLPEACH_FALLING_RIGHT");
+                } else if (direction == DIRECTION_LEFT) {
+                    sprite = &ResrcManager::GetInstance().getTexture("SMALLPEACH_FALLING_LEFT");
+                }
+            }
+            if (state == OBJECT_STATE_DYING) {
+                sprite = &ResrcManager::GetInstance().getTexture("PEACH_DIE");
+            }
+            if (state == OBJECT_STATE_VICTORY) {
+                sprite = &ResrcManager::GetInstance().getTexture("SMALLPEACH_VICTORY");
+            }
+            break;
+        }
+        case BIG:
+        {
+            if(state == OBJECT_STATE_ON_GROUND) {
+                if (vel.x != 0){
+                    if ( direction == DIRECTION_RIGHT) {
+                        if(currentFrame == 0) {
+                            sprite = &ResrcManager::GetInstance().getTexture("SUPER_PEACH_0_RIGHT");
+                        } else if(currentFrame == 1) {
+                            sprite = &ResrcManager::GetInstance().getTexture("SUPER_PEACH_1_RIGHT");
+                        } else if(currentFrame == 2) {
+                            sprite = &ResrcManager::GetInstance().getTexture("SUPER_PEACH_2_RIGHT");
+                        }
+                    }
+                    else if ( direction == DIRECTION_LEFT) {
+                        if(currentFrame == 0) {
+                            sprite = &ResrcManager::GetInstance().getTexture("SUPER_PEACH_0_LEFT");
+                        } else if(currentFrame == 1) {
+                            sprite = &ResrcManager::GetInstance().getTexture("SUPER_PEACH_1_LEFT");
+                        } else if(currentFrame == 2) {
+                            sprite = &ResrcManager::GetInstance().getTexture("SUPER_PEACH_2_LEFT");
+                        }
+                    }
+                }
+                if(vel.x == 0) {
+                    if ( direction == DIRECTION_RIGHT) {
+                        sprite = &ResrcManager::GetInstance().getTexture("SUPER_PEACH_0_RIGHT");
+                    } else if ( direction == DIRECTION_LEFT) {
+                        sprite = &ResrcManager::GetInstance().getTexture("SUPER_PEACH_0_LEFT");
+                    }
+                }
+            }
+
+            if (state == OBJECT_STATE_JUMPING) {
+                if (direction == DIRECTION_RIGHT) {
+                    sprite = &ResrcManager::GetInstance().getTexture("SUPER_PEACH_JUMPING_0_RIGHT");
+                } else if (direction == DIRECTION_LEFT) {
+                    sprite = &ResrcManager::GetInstance().getTexture("SUPER_PEACH_JUMPING_0_LEFT");
+                }
+            }
+
+            if (state == OBJECT_STATE_FALLING) {
+                if (direction == DIRECTION_RIGHT) {
+                    sprite = &ResrcManager::GetInstance().getTexture("SUPER_PEACH_FALLING_0_RIGHT");
+                } else if (direction == DIRECTION_LEFT) {
+                    sprite = &ResrcManager::GetInstance().getTexture("SUPER_PEACH_FALLING_0_LEFT");
+                }
+            }
+            if (state == OBJECT_STATE_DYING) {
+                sprite = &ResrcManager::GetInstance().getTexture("PEACH_DIE");
+            }
+            if (state == OBJECT_STATE_VICTORY) {
+                sprite = &ResrcManager::GetInstance().getTexture("SUPER_PEACH_VICTORY");
+            }
+            break;
+        }
+        case FIRE:
+        {
+            if(state == OBJECT_STATE_ON_GROUND) {
+                if (vel.x != 0){
+                    if ( direction == DIRECTION_RIGHT) {
+                        if(currentFrame == 0) {
+                            sprite = &ResrcManager::GetInstance().getTexture("FIRE_PEACH_0_RIGHT");
+                        } else if(currentFrame == 1) {
+                            sprite = &ResrcManager::GetInstance().getTexture("FIRE_PEACH_1_RIGHT");
+                        } else if(currentFrame == 2) {
+                            sprite = &ResrcManager::GetInstance().getTexture("FIRE_PEACH_2_RIGHT");
+                        }
+                    }
+                    else if ( direction == DIRECTION_LEFT) {
+                        if(currentFrame == 0) {
+                            sprite = &ResrcManager::GetInstance().getTexture("FIRE_PEACH_0_LEFT");
+                        } else if(currentFrame == 1) {
+                            sprite = &ResrcManager::GetInstance().getTexture("FIRE_PEACH_1_LEFT");
+                        } else if(currentFrame == 2) {
+                            sprite = &ResrcManager::GetInstance().getTexture("FIRE_PEACH_2_LEFT");
+                        }
+                    }
+                }
+                if(vel.x == 0) {
+                    if ( direction == DIRECTION_RIGHT) {
+                        sprite = &ResrcManager::GetInstance().getTexture("FIRE_PEACH_0_RIGHT");
+                    } else if ( direction == DIRECTION_LEFT) {
+                        sprite = &ResrcManager::GetInstance().getTexture("FIRE_PEACH_0_LEFT");
+                    }
+                }
+            }
+
+            if (state == OBJECT_STATE_JUMPING) {
+                if (direction == DIRECTION_RIGHT) {
+                    sprite = &ResrcManager::GetInstance().getTexture("FIRE_PEACH_JUMPING_0_RIGHT");
+                } else if (direction == DIRECTION_LEFT) {
+                    sprite = &ResrcManager::GetInstance().getTexture("FIRE_PEACH_JUMPING_0_LEFT");
+                }
+            }
+
+            if (state == OBJECT_STATE_FALLING) {
+                if (direction == DIRECTION_RIGHT) {
+                    sprite = &ResrcManager::GetInstance().getTexture("FIRE_PEACH_FALLING_0_RIGHT");
+                } else if (direction == DIRECTION_LEFT) {
+                    sprite = &ResrcManager::GetInstance().getTexture("FIRE_PEACH_FALLING_0_LEFT");
+                }
+            }
+
+            if (state == OBJECT_STATE_DYING) {
+                sprite = &ResrcManager::GetInstance().getTexture("PEACH_DIE");
+            }
+            if (state == OBJECT_STATE_VICTORY) {
+                sprite = &ResrcManager::GetInstance().getTexture("FIRE_PEACH_VICTORY");
+            }
+            break;
+        }
+    }
+}
+
+
+
