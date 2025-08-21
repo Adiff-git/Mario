cd1216cc3257589ef12d6278e90d9190b839d9f2
Yeennhy <yennhii06072006@gmail.com>
2025-08-21 22:24:54 +0700

    fdgfjkhljhjghg

--- src/Bob-omb.cpp
+++ src/Bob-omb.cpp
@@ -64,4 +64,4 @@ void Bob_omb::UpdateStateAndPhysic() {
 //ttydfhggsdhfgjdghfhgk
 //àghsfsahfafjyhtrdsfdfdgfhg
 //gdghfhsdfhjklhgfddxfghhkjgfdgfdhgdg
-//dsfdtyuujhghfsfdghjkhg
+//dsfdtyuujhghfsfdghjkhgfđgfjkhljhh
